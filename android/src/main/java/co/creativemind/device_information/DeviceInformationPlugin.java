1e4e0b625902b8f1985730a5c22cc38a9c9be025
Naji Ezzeldien <aboalnaja10@gmail.com>
2025-05-11 10:12:02 +0200

    Update 4 DeviceInformationPlugin.java

--- android/src/main/java/co/creativemind/device_information/DeviceInformationPlugin.java
+++ android/src/main/java/co/creativemind/device_information/DeviceInformationPlugin.java
@@ -85,18 +85,19 @@ public class DeviceInformationPlugin implements FlutterPlugin, MethodCallHandler
                 Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
             return Manifest.permission.READ_PHONE_STATE;
         } else {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) { // Android 10+
-                imeiNumber = Settings.Secure.getString(activity.getContentResolver(), Settings.Secure.ANDROID_ID); // alternative to getDeviceId()
-            } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                if (telephonyManager != null && telephonyManager.getImei() != null) {
-                    imeiNumber = telephonyManager.getImei();
-                }
-            } else {
-                if (telephonyManager != null && telephonyManager.getImei() != null) {
-                    imeiNumber = telephonyManager.getImei();
-                }
-            }
+      if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+        imeiNumber = getDeviceUniqueID();
+
+      }else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+        if (telephonyManager != null && telephonyManager.getImei() != null) {
+          imeiNumber = telephonyManager.getImei();
         }
+      }else {
+        if (telephonyManager != null && telephonyManager.getImei() != null) {
+          imeiNumber = telephonyManager.getImei();
+        }
+      }
+    }
         return imeiNumber;
     }
 
